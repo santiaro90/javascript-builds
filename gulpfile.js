ab49f83d5d6ea62bea3093e57942fb14a2f6601b
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2016-02-20 23:44:15 -0500

    Clean up task imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({lazy: true});
 
-var bundleTask = require('./gulp/tasks/bundle').bind(null, gulp, plugins);
-var cleanTask = require('./gulp/tasks/clean');
-var lintTask = require('./gulp/tasks/lint').bind(null, gulp, plugins);
-var stylesTask = require('./gulp/tasks/styles').bind(null, gulp, plugins);
+var bundle = require('./gulp/tasks/bundle').bind(null, gulp, plugins);
+var clean = require('./gulp/tasks/clean');
+var lint = require('./gulp/tasks/lint').bind(null, gulp, plugins);
+var styles = require('./gulp/tasks/styles').bind(null, gulp, plugins);
 
-gulp.task('bundle', ['clean', 'lint'], bundleTask);
-gulp.task('clean', cleanTask);
-gulp.task('lint', lintTask);
-gulp.task('styles', ['clean'], stylesTask);
+gulp.task('bundle', ['clean', 'lint'], bundle);
+gulp.task('clean', clean);
+gulp.task('lint', lint);
+gulp.task('styles', ['clean'], styles);
 
 gulp.task('build', ['bundle', 'styles'])
